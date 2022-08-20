36a7f13af96a28866c1a4052db0daaa18ea6241e
Egor Kisel <kisel1989@mail.ru>
2022-08-20 22:50:41 +0300

    hw3 | оператор flatMap

--- app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
+++ app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
@@ -1,6 +1,7 @@
 package com.geekbrains.rxjava2dagger2moxy
 
 import io.reactivex.rxjava3.core.Observable
+import kotlin.random.Random
 
 private val data = listOf<Int>(1, 2, 34, 685, 23, 53, 3, 2, 2, 45, 34, 1, 2, 90, 53)
 
@@ -9,10 +10,11 @@ fun main(){
     val observableNames = Observable.just("Egor", "Vitaliy", "Galia")
     val observableMail = Observable.just("Egor@mail.ru", "Vitaliy@yandex.ru", "Galia@gmail.com")
 
-    //mergeWith - объединяет два потока
     observableNames
-        .mergeWith(observableMail)
-        .subscribe{
+        .flatMap { element ->
+            //val delay = Random.nextInt(1000)
+            return@flatMap getUserInfo(element)
+        }.subscribe{
         println(it)
     }
 //
@@ -23,4 +25,8 @@ fun main(){
 //        .subscribe{
 //            println(it)
 //    }
+}
+
+private fun getUserInfo(name: String): Observable<List<String>>{
+    return Observable.just(listOf(name, "email.com"))
 }
