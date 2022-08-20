bd998bd24b5d01a22a3d469466f1861f53e14fbe
Egor Kisel <kisel1989@mail.ru>
2022-08-20 16:47:32 +0300

    hw3 | оператор fromCallable

--- app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
+++ app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
@@ -1,15 +1,21 @@
 package com.geekbrains.rxjava2dagger2moxy
 
 import io.reactivex.rxjava3.core.Observable
-import java.util.concurrent.TimeUnit
 
 private val data = listOf<Int>(1, 2, 34, 685, 23, 53, 3, 2, 2, 45, 34, 1, 2, 90, 53)
 
 fun main(){
 
-    //от и до какого элемента использовать наш поток
-    Observable.range(5, 8).subscribe {
+    Observable.fromCallable {
+        val result = old()
+        return@fromCallable result
+    }.subscribe{
         println(it)
     }
 
+}
+
+fun old(): List<Boolean> {
+    Thread.sleep(2000L)
+    return listOf(true, false, true)
 }
