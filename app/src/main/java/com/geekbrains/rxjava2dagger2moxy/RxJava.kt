ebb4ae823d39546d0518a2acdf554f3acfe527c7
Egor Kisel <kisel1989@mail.ru>
2022-08-20 16:35:01 +0300

    hw3 | оператор range

--- app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
+++ app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
@@ -1,17 +1,14 @@
 package com.geekbrains.rxjava2dagger2moxy
 
 import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 private val data = listOf<Int>(1, 2, 34, 685, 23, 53, 3, 2, 2, 45, 34, 1, 2, 90, 53)
 
 fun main(){
 
-    Observable.fromIterable(data).subscribe(
-        { element ->
-            println(element)
-        },
-        { error ->
+    Observable.range(5, 8).subscribe {
+        println(it)
+    }
 
-        }
-    )
 }
