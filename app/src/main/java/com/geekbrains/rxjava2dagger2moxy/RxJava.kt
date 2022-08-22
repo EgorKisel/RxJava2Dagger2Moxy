ce9fed29698cd5a8de0901d15ba1fb8ec84e6c60
Egor Kisel <kisel1989@mail.ru>
2022-08-22 13:08:01 +0300

    hw3 | аналоги Observable: Single, Maybe, Completable и Flowable

--- app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
+++ app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
@@ -1,6 +1,7 @@
 package com.geekbrains.rxjava2dagger2moxy
 
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.*
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 private val data = listOf<Int>(1, 2, 34, 685, 23, 53, 3, 2, 2, 45, 34, 1, 2, 90, 53)
@@ -12,8 +13,9 @@ fun main(){
 
     observableNames
         .flatMap { element ->
-            //val delay = Random.nextInt(1000)
+            val delay = Random.nextInt(1000)
             return@flatMap getUserInfo(element)
+                .delay(delay.toLong(), TimeUnit.MILLISECONDS)
         }.subscribe{
         println(it)
     }
@@ -25,6 +27,24 @@ fun main(){
 //        .subscribe{
 //            println(it)
 //    }
+
+    Single.create<Int>{
+        it.onSuccess(1)
+    }
+
+    Maybe.create<Int>{
+        it.onSuccess(1)
+        it.onComplete()
+    }
+
+    Completable.create{
+        it.onComplete()
+    }
+
+    Flowable.create<Int>({
+        //it.onNext()
+        it.onComplete()
+    }, BackpressureStrategy.DROP)
 }
 
 private fun getUserInfo(name: String): Observable<List<String>>{
