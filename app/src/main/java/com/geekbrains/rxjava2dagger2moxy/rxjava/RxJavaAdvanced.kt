b9b6a9c0c697c27c2c1dcfb9650a8e4cea9230cc
Egor Kisel <kisel1989@mail.ru>
2022-09-03 09:04:09 +0300

    hw4 | refCount()

--- app/src/main/java/com/geekbrains/rxjava2dagger2moxy/rxjava/RxJavaAdvanced.kt
+++ app/src/main/java/com/geekbrains/rxjava2dagger2moxy/rxjava/RxJavaAdvanced.kt
@@ -1,10 +1,8 @@
 package com.geekbrains.rxjava2dagger2moxy.rxjava
 
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.observables.ConnectableObservable
-import io.reactivex.rxjava3.schedulers.TestScheduler
-import java.util.concurrent.TimeUnit
+
 
 fun main(){
     val hotObservable = createHotObservable()
@@ -13,12 +11,13 @@ fun main(){
             println("first = $it")
         }
 
-    hotObservable.connect()
+   // hotObservable.connect()
 }
 
-fun createHotObservable(): ConnectableObservable<Int> {
+fun createHotObservable(): Observable<Int> {
 
     return Observable.just(1, 12, 23, 34, 45)
         .map { it.also { println(it) } }
-        .replay()
+        .publish()
+        .refCount()
 }
