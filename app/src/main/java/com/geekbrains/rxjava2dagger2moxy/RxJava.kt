8435944c61439aa2849c2556ff6c89c29e9f4c8c
Egor Kisel <kisel1989@mail.ru>
2022-09-03 07:12:00 +0300

    hw4 | andThen() у Completable и Single

--- app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
+++ app/src/main/java/com/geekbrains/rxjava2dagger2moxy/RxJava.kt
@@ -7,13 +7,30 @@ import io.reactivex.rxjava3.disposables.Disposable
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
+private val news = listOf("ВСУ взяли в плен россиян и разбили технику врага", "Глава МАГАТЭ Гросси обеспокоился состоянием Запорожской АЭС")
 
 fun main(){
 
     saveCompletable("kisel1989@mail.ru")
-        .subscribe{
-            println("Complete")
-        }
+        .andThen(getNews())
+        .subscribe({
+            println("Single subscribe")
+            println(it)
+        }, {
+            println(it.message)
+        })
+}
+
+private fun getNews() = Single.create<List<String>>{ emitter ->
+    val result = news
+    try {
+        emitter.onSuccess(result)
+    } catch (e: Exception){
+        Log.e("API_ERROR", e.message, e)
+        emitter.onError(e)
+    }
+
+
 }
 
 private fun saveCompletable(email: String) = Completable.create { emitter ->
