6a0efb8586b13b4e3391c35417004cb5701d839f
Egor Kisel <kisel1989@mail.ru>
2022-09-03 08:20:47 +0300

    hw4 | TestScheduler

--- app/src/main/java/com/geekbrains/rxjava2dagger2moxy/rxjava/RxJavaAdvanced.kt
+++ app/src/main/java/com/geekbrains/rxjava2dagger2moxy/rxjava/RxJavaAdvanced.kt
@@ -1,7 +1,10 @@
 package com.geekbrains.rxjava2dagger2moxy.rxjava
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.observables.ConnectableObservable
+import io.reactivex.rxjava3.schedulers.TestScheduler
+import java.util.concurrent.TimeUnit
 
 fun main(){
     val hotObservable = createHotObservable()
@@ -10,14 +13,16 @@ fun main(){
             println("first = $it")
         }
 
-    Thread.sleep(100)
-
-    hotObservable.subscribe{
-        println("second = $it")
-    }
-
     hotObservable.connect()
 }
 
-fun createHotObservable(): ConnectableObservable<Int> = Observable.just(11, 55, 4, 21)
-    .publish()
+fun createHotObservable(): ConnectableObservable<Int> {
+    val testScheduler = TestScheduler(1, TimeUnit.MINUTES)
+
+    return Observable.just(1, 12, 23, 34, 45)
+        .map { it.also { println(it) } }
+        .publish()
+        .also {
+            testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+        }
+}
