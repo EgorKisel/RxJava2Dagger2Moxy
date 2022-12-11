d7030b747c7cc3bcaa1732b4f13722b307dfbce4
Egor Kisel <kisel1989@mail.ru>
2022-12-11 15:04:03 +0300

    Mockito Implementation

--- app/src/test/java/com/geekbrains/rxjava2dagger2moxy/ExampleUnitTest.kt
+++ app/src/test/java/com/geekbrains/rxjava2dagger2moxy/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.geekbrains.rxjava2dagger2moxy
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
